3dd6d9242268916ffb49c6befea18af8da38bc34
Adar Azulay <adar@apiiro.com>
2024-08-04 12:07:01 +0300

    all relevent imports for tesing smoke test output

---
+++ TechForSmokeTest/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace TechForSmokeTest;
+
+using System.Security.Cryptography;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Text;
+using System.Security.Claims;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Docker.DotNet;
+
+public class Class1
+{
+}
